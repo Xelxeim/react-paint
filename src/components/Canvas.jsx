2909d8d7e877164c0df741522014a71bee853b44
Xelxeim <xelxeim@gmail.com>
2021-05-08 12:56:07 +0300

    Create MobX store and brush tool

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import { observer } from 'mobx-react-lite';
 
 import "../styles/canvas.scss";
+import canvasState from '../store/canvasState';
+import toolState from '../store/toolState';
+import Brush from '../tools/Brush';
 
-const Canvas = () => {
+const Canvas = observer(() => {
+  const canvasRef = useRef()
+  
+  useEffect(() => {
+    canvasState.setCanvas();
+    toolState.setTool(new Brush(canvasRef.current));
+  }, [])
+
   return (
     <div className="canvas">
-      <canvas width={800} height={600}></canvas>
+      <canvas ref={canvasRef} width={800} height={600} />
     </div>
   )
-}
+})
 
 export default Canvas;
---
+++ src/store/canvasState.js
@@ -0,0 +1,15 @@
+import { makeAutoObservable } from "mobx"
+
+class CanvasState {
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  canvas = null;
+
+  setCanvas(canvas) {
+    this.canvas = canvas
+  }
+}
+
+export default new CanvasState();
